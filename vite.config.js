48abf71f99f5f3e304ba2beddb3290e324fd31f8
dinaajaaa <dinaajaaa85@gmail.com>
2026-01-22 22:43:45 +0700

    Update file

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "/rzky-porto/",
+  base: "/react-portofolio-rizky-fadillah/",
   plugins: [
     tailwindcss(),
   ],
